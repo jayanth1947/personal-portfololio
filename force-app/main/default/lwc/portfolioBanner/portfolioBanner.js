36143cf62a8d6321fa9829ca5fa9280de31e59fc
jayanth1947 <jayanthonteru111@gmail.com>
2023-08-26 18:25:11 +0530

    modified the portfolioBanner cmp
    Data is fetcched from salesforce records

--- force-app/main/default/lwc/portfolioBanner/portfolioBanner.js
+++ force-app/main/default/lwc/portfolioBanner/portfolioBanner.js
@@ -1,5 +1,11 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import PortfolioAssets from '@salesforce/resourceUrl/PortfolioAssets';
+import { getRecord,getFieldValue } from 'lightning/uiRecordApi';
+import FULLNAME from '@salesforce/schema/Portfolio__c.FullName__c';
+import COMPANYLOCATION from '@salesforce/schema/Portfolio__c.CompanyLocation__c';
+import COMPANYNAME from '@salesforce/schema/Portfolio__c.CompanyName__c';
+import DESIGNATION from '@salesforce/schema/Portfolio__c.Designation__c';
+
 export default class PortfolioBanner extends LightningElement {
 
     linkedinUrl= 'https://www.linkedin.com/in/onteru-jayanth'
@@ -10,4 +16,37 @@ export default class PortfolioBanner extends LightningElement {
     linkedin =`${PortfolioAssets}/PortfolioAssets/Social/linkedin.svg`
     github =`${PortfolioAssets}/PortfolioAssets/Social/github.svg`
     trailhead =`${PortfolioAssets}/PortfolioAssets/Social/trailhead1.svg`
+
+    recordId='a0s5h000001uIjWAAU';
+    @wire(getRecord, {recordId:'$recordId', fields:[FULLNAME,COMPANYLOCATION,COMPANYNAME,DESIGNATION]})
+
+    portfolioData
+    // portfolioHandler({data,error}){
+    //     if(data){
+    //         console.log("record Data",JSON.stringify(data));
+    //     }
+    //     if (error) {
+    //         console.error("error",error);
+    //     }
+    // }
+
+
+    get fullName(){
+        return getFieldValue(this.portfolioData.data, FULLNAME)
+    }
+
+    get companyLocation(){
+        return getFieldValue(this.portfolioData.data, COMPANYLOCATION)
+    }
+
+    get companyName(){
+        return getFieldValue(this.portfolioData.data, COMPANYNAME)
+    }
+
+    get designation(){
+        return getFieldValue(this.portfolioData.data, DESIGNATION)
+    }
+
+    
+
 }
