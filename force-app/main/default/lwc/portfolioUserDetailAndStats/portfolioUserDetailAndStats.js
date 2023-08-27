8ae962d400b1f87dad9747e9110fba81a615ea98
jayanth1947 <jayanthonteru111@gmail.com>
2023-08-27 11:44:15 +0530

    added child cmp to parent cmp of UserDetailStats

--- force-app/main/default/lwc/portfolioUserDetailAndStats/portfolioUserDetailAndStats.js
+++ force-app/main/default/lwc/portfolioUserDetailAndStats/portfolioUserDetailAndStats.js
@@ -1,3 +1,6 @@
 import { LightningElement } from 'lwc';
 
-export default class PortfolioUserDetailAndStats extends LightningElement {}
+export default class PortfolioUserDetailAndStats extends LightningElement {
+    recordId="a0s5h000001uIjWAAU"
+    objectApiName="Portfolio__c"
+}
