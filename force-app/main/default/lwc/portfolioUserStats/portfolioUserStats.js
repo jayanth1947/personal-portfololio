34131291a0cd58de69a7f181e98658366e5064dd
jayanth1947 <jayanthonteru111@gmail.com>
2023-08-27 14:54:38 +0530

    added portfolioUserStats component

---
+++ force-app/main/default/lwc/portfolioUserStats/portfolioUserStats.js
@@ -0,0 +1,18 @@
+import { LightningElement, api } from 'lwc';
+import PortfolioAssets from '@salesforce/resourceUrl/PortfolioAssets';
+
+export default class PortfolioUserStats extends LightningElement {
+
+     trailheadRanking
+     @api badges
+     @api points
+     @api trails
+     @api rank
+
+     renderedCallback(){
+        if(this.rank){
+            let url=`${PortfolioAssets}/PortfolioAssets/Ranks/${this.rank}.png`
+            this.trailheadRanking=url
+        }
+     }
+}
