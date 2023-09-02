b85797ff9acac181224d9f64bbc96e0a7fe75ea8
jayanth1947 <jayanthonteru111@gmail.com>
2023-09-02 17:09:37 +0530

    added activity timeline to dispaly work Experience

--- force-app/main/default/lwc/portfolioWorkExperience/portfolioWorkExperience.js
+++ force-app/main/default/lwc/portfolioWorkExperience/portfolioWorkExperience.js
@@ -4,6 +4,8 @@ export default class PortfolioWorkExperience extends LightningElement {
 
     @api recordId;
 
+    workExperienceList
+
     @wire(getRelatedListRecords,{
        parentRecordId:'$recordId',
        relatedListId:'WorkExperience__r',
@@ -18,9 +20,33 @@ export default class PortfolioWorkExperience extends LightningElement {
     })workExperienceHandler({data,error}){
         if(data){
             console.log("Work Experience Data",JSON.stringify(data));
+            this.formattedData(data);
         }
         if(error){
             console.error(error);
         }
+    }
+
+    formattedData(data){
+        this.workExperienceList=[...data.records].reverse().map(item=>{
+            let id=item.id
+            const {CompanyName__c,Description__c,Is_Current__c,JobStartDate__c
+            ,JobEndDate__c,Role__c,WorkLocation__c} =item.fields
+
+            let CompanyName=this.getValue(CompanyName__c)
+            let Description=this.getValue(Description__c)
+            let IsCurrent=this.getValue(Is_Current__c)
+            let JobStartDate=this.getValue(JobStartDate__c)
+            let JobEndDate=this.getValue(JobEndDate__c)
+            let Role=this.getValue(Role__c)
+            let WorkLocation=this.getValue(WorkLocation__c)
+
+            return {id,CompanyName,Description,IsCurrent,JobStartDate,JobEndDate,Role,WorkLocation}
+        })
+        console.log("Work Experience ",JSON.stringify(this.workExperienceList));
+    }
+
+    getValue(data){
+        return data && (data.displayValue || data.value)
     }
 }
