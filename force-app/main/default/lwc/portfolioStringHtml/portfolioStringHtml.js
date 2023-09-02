b8d7c0f130199c5984933e491e7b74ee9cebcd26
jayanth1947 <jayanthonteru111@gmail.com>
2023-09-02 19:14:57 +0530

    added portfoliioStringHtml component

---
+++ force-app/main/default/lwc/portfolioStringHtml/portfolioStringHtml.js
@@ -0,0 +1,21 @@
+
+import { LightningElement, api } from 'lwc';
+
+export default class PortfolioStringHtml extends LightningElement {
+    @api content
+
+    isLoaded=false
+
+    renderedCallback(){
+        if(this.isLoaded){
+            return false
+        }
+
+        if(this.content){
+            this.isLoaded=true
+            const container =this.template.querySelector('.htmlContainer')
+            container.innerHTML=this.content
+
+        }
+    }
+}
