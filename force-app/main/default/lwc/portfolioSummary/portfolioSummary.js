bc994f99e9896044f715fccf433a54eb47295770
jayanth1947 <jayanthonteru111@gmail.com>
2023-09-10 19:02:52 +0530

    added comments and structure the code of PS cmp

--- force-app/main/default/lwc/portfolioSummary/portfolioSummary.js
+++ force-app/main/default/lwc/portfolioSummary/portfolioSummary.js
@@ -2,6 +2,8 @@ import { LightningElement,api } from 'lwc';
 
 export default class PortfolioSummary extends LightningElement {
 
+    // Expose the 'recordId' property as an API property to receive a record's ID
     @api recordId;
+    // Expose the 'objectApiName' property as an API property to receive an object's API name
     @api objectApiName;
 }
