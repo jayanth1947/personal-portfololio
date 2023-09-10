aea85d4a631b0c94c9b60d9cfb451c550b146465
jayanth1947 <jayanthonteru111@gmail.com>
2023-09-10 19:36:17 +0530

    added comments for PUD cmp

--- force-app/main/default/lwc/portfolioUserDetails/portfolioUserDetails.js
+++ force-app/main/default/lwc/portfolioUserDetails/portfolioUserDetails.js
@@ -1,10 +1,15 @@
 import { LightningElement, api } from 'lwc';
 
 export default class PortfolioUserDetails extends LightningElement {
+
+    // Expose the following properties to make them accessible in the parent component
     @api recordId
     @api objectApiName
     @api resumeUrl
+    
+    // Function to handle the downloadResume button click
     downloadResume(){
+        // Open the resume URL in a new tab or window
         window.open(this.resumeUrl,"_blank")
     }
 }
