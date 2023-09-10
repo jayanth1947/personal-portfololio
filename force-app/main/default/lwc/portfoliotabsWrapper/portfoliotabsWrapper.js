43a71121fb5268146f9b2a77c9359327ac517fac
jayanth1947 <jayanthonteru111@gmail.com>
2023-09-10 19:10:58 +0530

    added comments and structure the code od PW cmp

--- force-app/main/default/lwc/portfoliotabsWrapper/portfoliotabsWrapper.js
+++ force-app/main/default/lwc/portfoliotabsWrapper/portfoliotabsWrapper.js
@@ -2,6 +2,8 @@ import { LightningElement,api } from 'lwc';
 
 export default class PortfoliotabsWrapper extends LightningElement {
 
+    // The unique record identifier passed to this component
     @api recordId=""
+    // The API name of the object associated with the record
     @api objectApiName=""
 }
