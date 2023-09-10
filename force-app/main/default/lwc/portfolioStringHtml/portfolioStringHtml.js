43bd3f99fdab46e7bf9c60ebc4a4f222e8b0a386
jayanth1947 <jayanthonteru111@gmail.com>
2023-09-10 18:58:25 +0530

    added comments and structure the code of PSH cmp

--- force-app/main/default/lwc/portfolioStringHtml/portfolioStringHtml.js
+++ force-app/main/default/lwc/portfolioStringHtml/portfolioStringHtml.js
@@ -2,19 +2,24 @@
 import { LightningElement, api } from 'lwc';
 
 export default class PortfolioStringHtml extends LightningElement {
+
+    // Expose the 'content' property as an API property to receive HTML content
     @api content
 
+    // Expose the 'content' property as an API property to receive HTML content
     isLoaded=false
 
+    // Callback method executed when the component is rendered
     renderedCallback(){
+        // Check if the content has already been loaded to avoid multiple renderings
         if(this.isLoaded){
-            return false
+            return false  // Exit the method if content has already been loaded
         }
 
         if(this.content){
-            this.isLoaded=true
-            const container =this.template.querySelector('.htmlContainer')
-            container.innerHTML=this.content
+            this.isLoaded=true // Mark content as loaded 
+            const container =this.template.querySelector('.htmlContainer')  // Find the HTML container element in the template
+            container.innerHTML=this.content // Set the inner HTML of the container with the provided 'content'
 
         }
     }
