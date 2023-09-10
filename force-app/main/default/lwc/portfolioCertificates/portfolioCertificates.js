9f2073d4f7a842b3394c2db6eab6ecf392b95444
jayanth1947 <jayanthonteru111@gmail.com>
2023-09-10 18:26:38 +0530

    added comments and structure the code for PC cmp

--- force-app/main/default/lwc/portfolioCertificates/portfolioCertificates.js
+++ force-app/main/default/lwc/portfolioCertificates/portfolioCertificates.js
@@ -6,32 +6,41 @@ import PortfolioAssets from '@salesforce/resourceUrl/PortfolioAssets';
 
 export default class PortfolioCertificates extends LightningElement {
 
+    // Initialize arrays to store Salesforce and other certifications
     sfCertList=[]
     otherCertList=[]
+
+    // Define the certification logo URL using Salesforce Static Resources
     certLogo=`${PortfolioAssets}/PortfolioAssets/cert_logo.png`
+    
+    // Expose component properties as API properties to allow customization in the Lightning App Builder
     @api recordId
 
+    // Wire service to fetch record data based on the provided recordId and specified fields
     @wire(getRecord,{
         recordId:'$recordId',
         fields:[SALESFORCE_CERT_FIELDS,OTHER_CERT_FIELDS]
     })certsHandler({data,error}){
         if(data){
-            console.log('Certification Data',JSON.stringify(data))
+            //console.log('Certification Data',JSON.stringify(data))
             this.formatData(data);
         }
-
         if(error){
             console.error('Certification Error',error);
         }
     }
 
 
+    // Method to format and organize certification data
     formatData(data){
       const{SalesforceCertification__c ,OtherCertifications__c}= data.fields  
+
+      // Split Salesforce certifications and format them
       this.sfCertList=SalesforceCertification__c ? SalesforceCertification__c.value.split(';').map(item=>{
         return `Salesforce Certified ${item}`
       }):[]
 
+      // Split other certifications
       this.otherCertList= OtherCertifications__c ? OtherCertifications__c.value.split(','):[]
     
     }
